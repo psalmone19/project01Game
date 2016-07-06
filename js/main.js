8fe1035a303f27483dbc51f07bb86ef79ec8ab43
Egan Bernardino <eegun19@yahoo.com>
2016-07-06 09:03:45 -0700

    added title screen and function to move words from left to right

--- js/main.js
+++ js/main.js
@@ -1,29 +1,62 @@
-$(document).ready() {
+$(document).ready(function() {
+  console.log('hello!');
+
+// VARIABLES //
+  var $word = $('.word');
+
+  var library = [
+    'evaluative', 'cartographer', 'discomfit', 'psyche',
+    'prognosticate', 'winnow', 'embed', 'provenance',
+    'amok', 'relegate', 'leery', 'detach',
+    'cornucopia', 'protract', 'indenture', 'academia',
+    'disgorge', 'ethos', 'quaff', 'libation',
+    'eschew', 'expertly', 'relevance', 'pessimist',
+    'prod', 'compress', 'admnistrator', 'shoddy',
+    'resource', 'dwindle', 'revere', 'college',
+    'touchstone', 'articulate', 'goad', 'fraught',
+    'disparity', 'consequential', 'expend', 'temerity',
+    'erotic', 'analytical', 'revise', 'stroll',
+    'detached', 'nonetheless', 'challenging', 'reliance',
+    'navigate', 'sheaf', 'barrage', 'periodically',
+    'surround', 'transcendent', 'pretentious', 'exhilarating',
+    'synonymous', 'guy', 'astonish', 'quest',
+    'challenge', 'exaggerate', 'intermittent', 'radically',
+    'gruff', 'suggest', 'legacy', 'develop',
+    'salient', 'altering', 'thesis', 'detect',
+    'enhance', 'creator', 'ponder', 'aspire',
+    'affront', 'projection', 'precept', 'repress',
+    'raise', 'metaphor', 'requirement', 'confront',
+    'precipitate', 'grade', 'banish', 'job',
+    'probe', 'abstract', 'arduous', 'major',
+    'virtually', 'culture', 'proximity', 'conformity',
+    'predict', 'profound', 'tenure', 'hatch',
+    'volunteer', 'commend', 'chilly', 'realm'
+  ];
+
+  $
+
+  //
+  function randomWord(term) {
+    var term = math.floor(math.random() * library.length - .1)
+      return library[term];
+  };
+
+  // moves words from left to right
+  $('.start-button').click(function(event) {
+    $('#title-screen').hide(100);
+    $('.hidden').removeClass('hidden');
+    value = $word.css('left') === '100%' ? 0 : '100%';
+      $word.animate({left: value}, 8000);
+  });
+
+  // function alternate () {
+  //   $word.
+  // }
+
+
+});
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
